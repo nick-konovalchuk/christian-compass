c015b3c7929b692eda27120d01a068bd54c8cb8f
Nick Konovalchuk <nikolay.konovalchuk@mail.ru>
2024-05-09 12:23:54 +0300

    chat

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
 import streamlit as st
 from llama_cpp import Llama
 
-llm = Llama(
-    model_path="./Phi-3-mini-4k-instruct-q4.gguf",  # path to GGUF file
-    n_ctx=4096,
-    n_threads=8,
-    stream=True
-)
+
+@st.cache_resource
+def get_model():
+    return Llama(
+        model_path="./Phi-3-mini-4k-instruct-q4.gguf",
+        n_ctx=4096,
+        n_threads=8,
+        stream=True,
+        verbose=False
+    )
+
+
+st.title("Christian Compass")
+
+llm = get_model()
 
 user_message = st.chat_input()
 
 if user_message:
-    with st.chat_message("ai"):
-        output = llm.create_completion(
-            f"<|user|>\n{user_message}<|end|>\n<|assistant|>",
-            max_tokens=256,  # Generate up to 256 tokens
+    with st.chat_message("user", avatar='👤'):
+        st.write(user_message)
+    with st.chat_message("ai", avatar='🧭'):
+        output = llm.create_chat_completion(
+            [
+                {"role": "user", "content": user_message}
+            ],
             stop=["<|end|>"],
             stream=True
         )
-        st.write_stream(item['choices'][0]['text'] for item in output)
-
+        st.write_stream(item['choices'][0]['delta']['content'] for item in output if
+                        'content' in item['choices'][0]['delta'])
+        st.caption("AI can make mistakes. Please, check the references")
