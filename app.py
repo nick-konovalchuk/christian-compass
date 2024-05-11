3a7bbd1d01f834b51bd5067dd4d8f336e4f390e7
Nick Konovalchuk <nikolay.konovalchuk@mail.ru>
2024-05-11 15:23:24 +0300

    kinda fixed blocking input

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ def get_chat_rag_agent():
     return LlamaCPPChatEngine("Phi-3-mini-4k-instruct-q4.gguf")
 
 
+def calc_progress_perc():
+    return min(round(st.session_state["ctx_len"] / chat_rag_agent.n_ctx * 100), 100)
+
+
 def pbar_callback():
-    pbar.progress(min(st.session_state["ctx_len"] / chat_rag_agent.n_ctx, 1), "Chat history limit")
+    pbar.progress(calc_progress_perc(), "Chat history limit")
 
 
 chat_rag_agent = get_chat_rag_agent()
@@ -20,32 +24,32 @@ if "messages" not in st.session_state or st.sidebar.button("Clear chat history")
     st.session_state["messages"] = []
     st.session_state["ctx_len"] = 0
 
-pbar = st.sidebar.progress(
-    min(st.session_state["ctx_len"] / chat_rag_agent.n_ctx, 1), "Chat history limit"
-)
+pbar = st.sidebar.progress(calc_progress_perc(), "Chat history limit")
 
 user_message = st.chat_input(disabled=st.session_state["input_blocked"])
 if user_message:
-    message_generator, n_tokens = chat_rag_agent.chat(st.session_state["messages"], user_message)
-    st.session_state["ctx_len"] += n_tokens
-    st.session_state["messages"].append(
-        {
-            "role": "user",
-            "content": user_message
-        }
-    )
+    if not st.session_state["input_blocked"]:
+        message_generator, n_tokens = chat_rag_agent.chat(
+            st.session_state["messages"],
+            user_message
+        )
+        st.session_state["ctx_len"] = n_tokens
+        st.session_state["messages"].append(
+            {
+                "role": "user",
+                "content": user_message
+            }
+        )
     render_chat_history()
-    pbar_callback()
-
-    message, finish_reason = get_render_assistant_message(message_generator, pbar_callback)
-    st.session_state["messages"].append(
-        {
-            "role": "assistant",
-            "content": message
-        }
-    )
+    if not st.session_state["input_blocked"]:
+        pbar_callback()
+        message, finish_reason = get_render_assistant_message(message_generator, pbar_callback)
+        st.session_state["messages"].append(
+            {
+                "role": "assistant",
+                "content": message
+            }
+        )
 if st.session_state["ctx_len"] >= chat_rag_agent.n_ctx:
     st.session_state["input_blocked"] = True
-    user_message = st.chat_input(disabled=st.session_state["input_blocked"])
     st.info("Chat history limit reached")
-
